2ab711d5a1e93beb0ae7e4fc48ca4ff18bded727
James Fredley <jamesfredley@users.noreply.github.com>
2024-11-20 17:00:02 -0500

    Update to grails 6.2.3-SNAPSHOT and grails-gradle-plugin 6.2.4-SNAPSHOT

--- grails-forge-core/src/test/groovy/org/grails/forge/feature/grails/GrailsGradlePluginSpec.groovy
+++ grails-forge-core/src/test/groovy/org/grails/forge/feature/grails/GrailsGradlePluginSpec.groovy
@@ -16,8 +16,8 @@ class GrailsGradlePluginSpec extends BeanContextSpec implements CommandOutputFix
         final String gradleProps = output["gradle.properties"]
 
         then:
-        gradleProps.contains("grailsGradlePluginVersion=6.2.3")
-        gradleProps.contains("grailsVersion=6.2.2")
+        gradleProps.contains("grailsGradlePluginVersion=6.2.4-SNAPSHOT")
+        gradleProps.contains("grailsVersion=6.2.3-SNAPSHOT")
     }
 
     void "test dependencies are present for buildSrc"() {
@@ -26,7 +26,7 @@ class GrailsGradlePluginSpec extends BeanContextSpec implements CommandOutputFix
                 .renderBuildSrc()
 
         then:
-        template.contains('implementation "org.grails:grails-gradle-plugin:6.2.3"')
+        template.contains('implementation "org.grails:grails-gradle-plugin:6.2.4-SNAPSHOT"')
     }
 
     void "test buildSrc is present for buildscript dependencies"() {
@@ -36,7 +36,7 @@ class GrailsGradlePluginSpec extends BeanContextSpec implements CommandOutputFix
 
         expect:
         buildGradle != null
-        buildGradle.contains("classpath \"org.grails:grails-gradle-plugin:6.2.3\"")
+        buildGradle.contains("classpath \"org.grails:grails-gradle-plugin:6.2.4-SNAPSHOT\"")
 
     }
 
